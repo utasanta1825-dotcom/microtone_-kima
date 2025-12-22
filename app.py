670e86ed18cf8d4531f88d88528da95e11bf623e
utasanta1825-dotcom <utasanta1825@gmail.com>
2025-12-23 02:21:58 +0900

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ import pandas as pd
 # =========================
 # 設定
 # =========================
-BASE_DIR = "microtone"         # ここはあなたのフォルダ名に合わせる
+BASE_DIR = "微分音"         # ここはあなたのフォルダ名に合わせる
 SINGLE_DIR = os.path.join(BASE_DIR, "single")
 CHORD_DIR  = os.path.join(BASE_DIR, "chord")
 
